a123b13024b10a5a9cddf6cdb926c9eb22a970a9
Grigory Mishin <uttuer@gmail.com>
2021-11-09 02:16:00 +0300

    git-rebase 1 Rebased Rebased

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,16 +2,13 @@
 # display command line options
 
 count=1
-<<<<<<< HEAD
+
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    
+    echo "Next parameter: $param"
+
     count=$(( $count + 1 ))
 done
 
 echo "====="
-=======
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
->>>>>>> git-merge
+
